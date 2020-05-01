8a10809c86a3a20527087f78f03b651fc6ee405a
SnowOnVolcano <541359373@qq.com>
2020-05-02 02:22:17 +0800

    add course's get

--- project/vue2-forntend/src/api/course.js
+++ project/vue2-forntend/src/api/course.js
@@ -2,8 +2,26 @@ import request from '@/utils/request'
 
 export function getUserCourses(uid) {
   return request({
-    url: '/vue-admin-template/api/user/courses',
+    url: '/user/${uid}/courses',
     method: 'get',
-    params: { uid }
+    uid
+  })
+}
+
+export function getCourseByCid(uid, cid) {
+  return request({
+    url: '/user/${uid}/course/${cid}/tasks',
+    method: 'get',
+    uid,
+    cid
+  })
+}
+
+export function getResourceByCid(uid, cid) {
+  return request({
+    url: '/user/${uid}/course/${cid}/resources',
+    method: 'get',
+    uid,
+    cid
   })
 }
--- project/vue2-forntend/src/views/Course/index.vue
+++ project/vue2-forntend/src/views/Course/index.vue
@@ -26,6 +26,7 @@
                 :items="ddls"
                 class="elevation-1"
                 show-expand
+                single-expand=true
                 :expanded.sync="expanded"
                 item-key="tid"
               >
@@ -33,9 +34,9 @@
                   <v-chip :color="getColor(item.remains)" dark>{{ item.remains }}</v-chip>
                 </template-->
 
-                <!--template v-slot:item.glutenfree="{ item }">
-                  <v-simple-checkbox v-model="item.glutenfree"></v-simple-checkbox>
-                </template-->
+                <template v-slot:item.is_finished="{ item }">
+                  <v-simple-checkbox v-model="item.is_finished"></v-simple-checkbox>
+                </template>
 
                 <template v-slot:expanded-item="{ headers, item }">
                   <td :colspan="headers.length">{{ item.content }}</td>
@@ -53,15 +54,15 @@
                     <tr>
                       <th class="text-left">资源名称</th>
                       <th class="text-left">提取码</th>
-                      <th class="text-left">分享人</th>
+                      <!--th class="text-left">分享人</th-->
                     </tr>
                   </thead>
                   <tbody>
-                    <tr v-for="item in srcs" :key="item.name">
-                      <td><a v-bind:href="item.url"> {{item.name}}</a></td>
-                      <td v-if="item.pss!=''">{{ item.pss }}</td>
+                    <tr v-for="item in srcs" :key="item.rid">
+                      <td><a v-bind:href="item.url"> {{item.title}}</a></td>
+                      <td v-if="item.code!=''">{{ item.code }}</td>
                       <td v-else></td>
-                      <td>{{ item.sharer }}</td>
+                      <!--td>{{ item.sharer }}</td-->
                     </tr>
                   </tbody>
                 </template>
@@ -98,6 +99,7 @@
 <script>
   import { getUserCourses } from '@/api/course';
   import { getCourseByCid } from '@/api/course';
+  import { getResourceByCid } from '@/api/course';
   import Eventlist from '../Eventlist/index.vue'
   //DDL列表 开始
   //DDL列表 结束
@@ -114,19 +116,19 @@
           text: '事项名称',
           align: 'start',
           sortable: false,
-          value: 'name',
+          value: 'title',
         },
-        //{ text: '发布时间', value: 'starts' },
-        { text: '截止时间', value: 'ddl.ddl_time' },
+        { text: '发布时间', value: 'create_time' },
+        { text: '截止时间', value: 'ddl_time' },
         //{ text: '剩余时间', value: 'remains' },
-        //{ text: '完成状态', value: 'glutenfree' },
-        { text: '已完成人数', value: 'dalaos' },
+        { text: '完成状态', value: 'is_finished' },
+        //{ text: '已完成人数', value: 'dalaos' },
         { text: '更多', value: 'data-table-expand' },
       ],
-      ddls: [],
-      ////////////////////////////////////DDL列表 结束
       tags: [],
-
+      ddls: [],
+      expends: [],
+      ////////////////////////////////////DDL列表 结束
       srcs: [],
       fileSubmit: {
         ftitle: '',
@@ -148,11 +150,9 @@
       initialize () {
         getUserCourses(this.$store.getters.uid).then(res => {
           console.log(res)
-          if (res.data.success) {
-            var r_courses = res.data.courses;
-            for (let i = 0; i < r_courses.data.length; i++) {
-              this.tags.push(r_courses.data[i]);
-            }
+          var r_courses = res.courses;
+          for (let i = 0; i < r_courses.length; i++) {
+            this.tags.push(r_courses[i]);
           }
         })
       },
@@ -161,55 +161,25 @@
         this.updatePage(item.cid);
       },
       updatePage (cid) {
-        this.ddls = [
-          {
-            name: '计算机科学方法论',
-            starts: 159,
-            ends: 6.0,
-            remains: 24,
-            contacts: 4.0,
-            glutenfree: false,
-            dalaos: 22,
-            contents: '计算机科学方法论作业具体内容',
-          },
-          {
-            name: '软件工程（罗杰、任建）',
-            starts: 237,
-            ends: 9.0,
-            remains: 200,
-            contacts: 4.3,
-            glutenfree: false,
-            dalaos: 66,
-            contents: '软件工程作业具体内容',
-          },
-        ]
+        this.ddls = []
+        getCourseByCid(this.$store.getters.uid, cid).then(res => {
+          console.log(res)
+          var r_tasks = res.tasks;
+          for (let i = 0; i < r_tasks.length; i++) {
+            this.ddls.push(r_tasks[i]);
+          }
+        })
 
-        // getCourseByCid(cid).then(res => {
-        //   console.log(res)
-        //   if (res.data.success) {
-        //     var r_tasks = res.data.tasks;
-        //     for (let i = 0; i < r_tasks.data.length; i++) {
-        //       this.ddls.push(r_tasks.data[i]);
-        //     }
-        //   }
-        // })
+        this.srcs = []
+        getResourceByCid(this.$store.getters.uid, cid).then(res => {
+          console.log(res)
+          var r_resources = res.resources;
+          for (let i = 0; i < r_resources.length; i++) {
+            this.srcs.push(r_resources[i]);
+          }
+        })
 
-        this.srcs = [
-          {
-            name: '计算机网络课程笔记',
-            url: 'https://www.buaa.edu.cn',
-            pss: '',
-            sharer: '小小软',
-          },
-          {
-            name: '软件工程学习资料',
-            url: 'https://pan.baidu.com',
-            pss: '2020',
-            sharer: '大大软',
-          },
-        ]
-        var i
-        for (i=0;i<this.ddls.length;i++) {
+        for (let i=0;i<this.ddls.length;i++) {
           this.courses[i] = this.ddls[i].name
           console.log(this.courses)
         }
